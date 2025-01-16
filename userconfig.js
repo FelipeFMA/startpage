452d9fadea6d9b315beb979a803821ba8dfa797d
FelipeFMA <felipefmavelar@gmail.com>
2025-01-16 10:29:13 -0300

    Changed location for weather

--- userconfig.js
+++ userconfig.js
@@ -1,10 +1,10 @@
 // latte / frappe / macchiato / mocha
-const palette = macchiato;
+const palette = mocha;
 
 const default_config = {
   overrideStorage: true,
   temperature: {
-    location: "London",
+    location: "Franca, SP",
     scale: "C",
   },
   clock: {
